8b0d7fcfb4ab7a485aae26deb6818c2b076d8e99
Joey Jirasevijinda <joey.jira@gmail.com>
2019-04-04 23:38:13 -0700

    Mocked current location in google maps lite

--- app/src/main/java/com/joey/android/mapbox/FriendBoxListFragment.java
+++ app/src/main/java/com/joey/android/mapbox/FriendBoxListFragment.java
@@ -1,6 +1,7 @@
 package com.joey.android.mapbox;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -15,10 +16,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
@@ -31,8 +40,37 @@ public class FriendBoxListFragment extends Fragment {
             Manifest.permission.ACCESS_FINE_LOCATION
     };
     private static final int REQUEST_LOCATION_PERMISSIONS = 0;
+    private static final int REQUEST_IMAGE_CODE = 1;
 
     private RecyclerView mFriendBoxRecyclerView;
+
+    private View.OnClickListener locationOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (hasLocationPermission()) {
+                findLocation();
+            } else {
+                requestPermissions(LOCATION_PERMISSIONS,
+                        REQUEST_LOCATION_PERMISSIONS);
+            }
+        }
+    };
+
+    private View.OnClickListener friendImageOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Create an Intent with action as ACTION_PICk
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            // Sets the type as image/*. This ensures only components of type image are selected
+            intent.setType("image/*");
+            // We pass an extra array with the accepted mime types.
+            // This will ensure only components with these MIME types as targets.
+            String[] mimeTypes = {"image/jpeg", "image/png"};
+            intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+            // Launching the Intent
+            startActivityForResult(intent, REQUEST_IMAGE_CODE);
+        }
+    };
 
     public static FriendBoxListFragment newInstance() {
         return new FriendBoxListFragment();
@@ -45,7 +83,8 @@ public class FriendBoxListFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_friend_box_list, container, false);
 
         mFriendBoxRecyclerView = view.findViewById(R.id.map_box_recycler_view);
@@ -89,35 +128,59 @@ public class FriendBoxListFragment extends Fragment {
         return result == PackageManager.PERMISSION_GRANTED;
     }
 
-    private class FriendBoxHolder extends RecyclerView.ViewHolder {
+    private class FriendBoxHolder extends RecyclerView.ViewHolder
+            implements OnMapReadyCallback {
         private FriendBox mFriendBox;
+        private GoogleMap mGoogleMap;
 
         private TextView mFriendNameTextView;
         private Button mLocationButton;
-
-        private View.OnClickListener locationOnClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (hasLocationPermission()) {
-                    findLocation();
-                } else {
-                    requestPermissions(LOCATION_PERMISSIONS,
-                            REQUEST_LOCATION_PERMISSIONS);
-                }
-            }
-        };
+        private MapView mMapView;
+        private ImageView mFriendImageView;
 
         public FriendBoxHolder(View friendBoxView) {
             super(friendBoxView);
 
             mFriendNameTextView = friendBoxView.findViewById(R.id.friend_name);
+
             mLocationButton = friendBoxView.findViewById(R.id.button_get_location);
             mLocationButton.setOnClickListener(locationOnClickListener);
+
+            mFriendImageView = friendBoxView.findViewById(R.id.friend_pic);
+            mFriendImageView.setOnClickListener(friendImageOnClickListener);
+
+            mMapView = friendBoxView.findViewById(R.id.friend_box_map_view);
+            Log.i(TAG, "" + mMapView);
+            if (mMapView != null) {
+                // Initialise the MapView
+                mMapView.onCreate(null);
+                // Set the map ready callback to receive the GoogleMap object
+                mMapView.getMapAsync(this);
+            }
         }
 
         public void bind(FriendBox friendBox) {
             mFriendBox = friendBox;
             mFriendNameTextView.setText(friendBox.getName());
+
+            GoogleMap map = mGoogleMap;
+            if (map != null) {
+
+            }
+        }
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            Log.i(TAG, "" + FriendBoxListFragment.this.getActivity());
+            MapsInitializer.initialize(FriendBoxListFragment.this.getActivity());
+            mGoogleMap = googleMap;
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.448849,-121.898045), 13f));
+            mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(37.448849,-121.898045)));
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+
+        private void setMapLocation() {
+
         }
     }
 
