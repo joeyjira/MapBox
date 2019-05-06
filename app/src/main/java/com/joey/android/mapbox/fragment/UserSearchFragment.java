977ea4790d31fe45fdf7d336d22b3f9b1ed2d256
Joey Jirasevijinda <joey.jira@gmail.com>
2019-05-06 16:17:49 -0700

    Blurred image in background behind user profile

--- app/src/main/java/com/joey/android/mapbox/fragment/UserSearchFragment.java
+++ app/src/main/java/com/joey/android/mapbox/fragment/UserSearchFragment.java
@@ -1,13 +1,22 @@
 package com.joey.android.mapbox.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,21 +29,30 @@ import com.joey.android.mapbox.R;
 import com.joey.android.mapbox.firebase.MapBoxFBSchema.Reference;
 import com.joey.android.mapbox.model.User;
 
-public class SearchUserFragment extends FirebaseFragment {
-    private static final String TAG = "SearchUserFragment";
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserSearchFragment extends FirebaseFragment {
+    private static final String TAG = "UserSearchFragment";
 
     private DatabaseReference mReference;
     private User mUser;
 
     private SearchView mEmailSearchView;
     private LinearLayout mUserProfileLayout;
-    private Button mSearchUserButton;
+    private CircleImageView mProfileImage;
+    private ImageView mBlurryImage;
     private TextView mUserNameText;
-    private TextView mUserEmail;
+    private TextView mUserStatusText;
     private Button mResponseButton;
 
-    public static SearchUserFragment newInstance() {
-        return new SearchUserFragment();
+    public static UserSearchFragment newInstance() {
+        return new UserSearchFragment();
     }
 
     @Override
@@ -48,20 +66,21 @@ public class SearchUserFragment extends FirebaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_search_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_search, container, false);
 
         mEmailSearchView = view.findViewById(R.id.edit_email);
+        mEmailSearchView.setOnQueryTextListener(userQueryListener);
 
         mUserProfileLayout = view.findViewById(R.id.layout_user_profile);
 
-        mSearchUserButton = view.findViewById(R.id.fragment_add_friend_get_user);
-        mSearchUserButton.setOnClickListener(getUserOnClickListener);
+        mProfileImage = view.findViewById(R.id.fragment_user_search_image);
+        mBlurryImage = view.findViewById(R.id.fragment_user_search_blurry);
 
-        mResponseButton = view.findViewById(R.id.fragment_add_friend_response);
+        mUserNameText = view.findViewById(R.id.fragment_user_search_name);
+        mUserStatusText = view.findViewById(R.id.fragment_user_search_status);
+
+        mResponseButton = view.findViewById(R.id.fragment_user_search_response);
         mResponseButton.setOnClickListener(addUserOnClickListener);
-
-        mUserNameText = view.findViewById(R.id.fragment_add_friend_name);
-        mUserEmail = view.findViewById(R.id.fragment_add_friend_email);
 
         return view;
     }
@@ -85,15 +104,6 @@ public class SearchUserFragment extends FirebaseFragment {
         return email.replace(".", ",");
     }
 
-    View.OnClickListener getUserOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            String email = mEmailSearchView.getQuery().toString().trim().toLowerCase();
-
-            searchUser(email);
-        }
-    };
-
     View.OnClickListener addUserOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -103,18 +113,35 @@ public class SearchUserFragment extends FirebaseFragment {
         }
     };
 
+    SearchView.OnQueryTextListener userQueryListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String s) {
+            String email = mEmailSearchView.getQuery().toString().trim().toLowerCase();
+            searchUser(email);
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String s) {
+            return false;
+        }
+    };
+
     ValueEventListener getUserListener = new ValueEventListener() {
         @Override
         public void onDataChange(@NonNull final DataSnapshot userSnapshot) {
+            // Check that user has signed up for app before
             if (userSnapshot.exists()) {
+                // Retrieve user from corresponding email
                 mReference.child(Reference.USERS)
                         .child(userSnapshot.getValue().toString())
                         .addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                                 mUser = dataSnapshot.getValue(User.class);
+                                new DownloadProfileImageTask().execute(mUser);
                                 mUserNameText.setText(mUser.getName());
-                                mUserEmail.setText(mUser.getEmail());
+                                mUserStatusText.setText(mUser.getEmail());
                             }
 
                             @Override
@@ -123,20 +150,23 @@ public class SearchUserFragment extends FirebaseFragment {
                             }
                         });
 
-                // Check if the email user searched refers to himself/herself
+                // Check if the email user searched refers own email
                 if (!userSnapshot.getValue().toString().equals(getUid())) {
                     mReference.child(Reference.FRIEND_REQUESTS)
                             .child(userSnapshot.getValue().toString())
                             .addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
                                 public void onDataChange(@NonNull DataSnapshot friendRequestSnapshot) {
+                                    // Check if there is a pending request already
                                     if (friendRequestSnapshot.exists()) {
+                                        // Update buttons if request is pending
+                                        mUserStatusText.setText("has not responded to your request yet");
                                         mResponseButton.setText(R.string.pending_request);
                                         mResponseButton.setEnabled(false);
-                                        mResponseButton.setVisibility(View.VISIBLE);
                                         return;
                                     }
 
+                                    // Check if users are friends already
                                     mReference.child(Reference.FRIENDS)
                                             .child(userSnapshot.getValue().toString())
                                             .child(getUid())
@@ -144,14 +174,13 @@ public class SearchUserFragment extends FirebaseFragment {
                                                 @Override
                                                 public void onDataChange(@NonNull DataSnapshot friendsSnapshot) {
                                                     if (friendsSnapshot.exists()) {
-                                                        mResponseButton.setText(R.string.existing_friend);
+                                                        mUserStatusText.setText(R.string.existing_friend);
                                                         mResponseButton.setEnabled(false);
                                                     } else {
                                                         mResponseButton.setText(R.string.add_friend);
                                                         mResponseButton.setEnabled(true);
+                                                        mResponseButton.setVisibility(View.VISIBLE);
                                                     }
-
-                                                    mResponseButton.setVisibility(View.VISIBLE);
                                                 }
 
                                                 @Override
@@ -167,9 +196,8 @@ public class SearchUserFragment extends FirebaseFragment {
                                 }
                             });
                 } else {
-                    mResponseButton.setText("This is you!");
+                    mUserStatusText.setText(R.string.own_email);
                     mResponseButton.setEnabled(false);
-                    mResponseButton.setVisibility(View.VISIBLE);
                 }
             } else {
                 // Make a toast, user cannot be found
@@ -183,4 +211,85 @@ public class SearchUserFragment extends FirebaseFragment {
 
         }
     };
+
+    public class DownloadProfileImageTask extends AsyncTask<User, Void, Bitmap> {
+        User mUser;
+
+        @Override
+        protected Bitmap doInBackground(User... users) {
+            Log.i(TAG, "Async Task called to download image");
+            mUser = users[0];
+
+            String photoUri = mUser.getPhotoUri();
+            Bitmap photoBitmap = null;
+
+            try {
+                byte[] imageBytes = getUrlBytes(photoUri);
+                photoBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch bitmap from url");
+            }
+
+            return photoBitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            mProfileImage.setImageBitmap(bitmap);
+            Bitmap blurImage = blur(bitmap);
+            mBlurryImage.setImageBitmap(blurImage);
+        }
+
+        private static final float BLUR_RADIUS = 15f;
+        private static final float BITMAP_SCALE = 0.4f;
+
+        public Bitmap blur(Bitmap image) {
+            if (null == image) return null;
+
+            int width = Math.round(image.getWidth() * BITMAP_SCALE);
+            int height = Math.round(image.getHeight() * BITMAP_SCALE);
+
+            Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
+            Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+            RenderScript rs = RenderScript.create(getActivity());
+            ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+            Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
+            Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
+            theIntrinsic.setRadius(BLUR_RADIUS);
+            theIntrinsic.setInput(tmpIn);
+            theIntrinsic.forEach(tmpOut);
+            tmpOut.copyTo(outputBitmap);
+
+            return outputBitmap;
+        }
+
+        private byte[] getUrlBytes(String urlSpec) throws IOException {
+            URL url = new URL(urlSpec);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            try {
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                InputStream in = connection.getInputStream();
+
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    throw new IOException(connection.getResponseMessage() +
+                            ": with " +
+                            urlSpec);
+                }
+
+                int bytesRead;
+                byte[] buffer = new byte[1024];
+
+                while ((bytesRead = in.read(buffer)) > 0) {
+                    out.write(buffer, 0, bytesRead);
+                }
+
+                out.close();
+                return out.toByteArray();
+            } finally {
+                connection.disconnect();
+            }
+        }
+    }
 }
