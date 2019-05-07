f46700270143d10b895822522e7c5d1b16f3bbd0
Joey Jirasevijinda <joey.jira@gmail.com>
2019-05-07 14:55:40 -0700

    Changed theme name

--- app/src/main/java/com/joey/android/mapbox/activity/SignInActivity.java
+++ app/src/main/java/com/joey/android/mapbox/activity/SignInActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -45,7 +44,7 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.AppTheme);
+        setTheme(R.style.MapBoxTheme);
         setContentView(R.layout.activity_sign_in);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
